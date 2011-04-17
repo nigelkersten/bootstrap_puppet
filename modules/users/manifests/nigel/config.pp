5b9e74948e2c3fde71f88c5ffba200173e290d5d
Nigel Kersten <nigel@puppetlabs.com>
2011-04-17 13:38:58 -0700

    set up vim

--- modules/users/manifests/nigel/config.pp
+++ modules/users/manifests/nigel/config.pp
@@ -1,9 +1,32 @@
 class users::nigel::config {
 
-  File { owner => nigel, group => nigel, mode => 0644, }
+  $home = "/home/nigel"
+  $src  = "${home}/src"
 
-  file { "/home/nigel/.vimswp":
+  File {
+        owner   => nigel,
+        group   => nigel,
+        mode    => 0644,
+        require => Vcsrepo["${src}/dotfiles",
+       }
+
+  vcsrepo { "${src}/dotfiles":
+    ensure   => present,
+    provider => "git",
+    source   => "git://github.com/nigelkersten/dotfiles.git",
+  }
+
+  file { "${home}/.vimswp":
     ensure => directory,
   }
 
+  file { "${home}/.vimrc":
+    ensure => symlink,
+    target => "${src}/dotfiles/vimrc",
+  }
+
+  file { "${home}/.vim":
+    ensure => symlink,
+    target => "${src}/dotfiles/vim",
+  }
 }
