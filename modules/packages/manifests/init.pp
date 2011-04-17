ba17eb45a30080594c48b6ca200e2899505fa37c
Nigel Kersten <nigel@puppetlabs.com>
2011-04-17 14:48:28 -0700

    more packages

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -1,10 +1,19 @@
 class packages {
 
-  package { "vim":
-    ensure => latest,
-    name   => $operatingsystem ? {
-      "centos" => "vim-enhanced",
-      "debian" => "vim",
-    },
+Package { ensure => latest, }
+
+$pkgs_common = []
+$pkgs_redhat = ["vim-enhanced", "ruby-shadow", ]
+$pkgs_debian = ["vim", "libshadow-ruby1.8", ]
+
+package { $pkgs_common: }
+
+case $operatingsystem {
+  redhat, centos: {
+    package { $pkgs_redhat: }
+  }
+  debian: {
+    package { $pkgs_debian: }
   }
 }
+
